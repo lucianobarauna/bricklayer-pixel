682b9ffb3cf44769670885902a913c08be92e2d6
Luciano Barauna [Fattoria] <luciano@FattoriaWebs-MacBook-Pro-2.local>
2016-11-24 12:07:35 -0200

    Corrigindo tarefas

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,16 @@ var gulp = require("gulp"),
 require(config.tasksPath + '/taskClean')(gulp, plugins, config);
 
 // Cria diretorio raiz e copia favicon
-require(config.tasksPath + '/taskFavico')(gulp, plugins, config);
+require(config.tasksPath + '/taskBuildFavico')(gulp, plugins, config);
 
 // Copia e otimiza imagens
 require(config.tasksPath + '/taskBuildImg')(gulp, plugins, config);
 
 // Copia fontes
-require(config.tasksPath + '/taskCopyFont')(gulp, plugins, config);
+require(config.tasksPath + '/taskBuildFont')(gulp, plugins, config);
 
 // Minifica e concatena
-require(config.tasksPath + '/taskBuildUsemin')(gulp, plugins, config);
+require(config.tasksPath + '/taskBuildCode')(gulp, plugins, config);
 
 // Start server
 require(config.tasksPath + '/taskServer')(gulp, plugins, config);
