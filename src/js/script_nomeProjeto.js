7ca4510216de323e8c89ae8703e968407b4f0871
Luciano Barauna [Fattoria] <luciano@FattoriaWebs-MacBook-Pro-2.local>
2016-11-28 10:33:46 -0200

    Removendo fontes desnecessárias para o projeto

--- src/js/script_nomeProjeto.js
+++ src/js/script_nomeProjeto.js
@@ -1,9 +1,7 @@
 /*!
- * Nome projeto
- * Desenvolvido em: 21/10/2016
+ * Scripts Solucao Aqui
+ * Desenvolvido por fattoriaweb em: 21/10/2016
  */
 $(function () {
-    'user strict'
-    var casa = 'Pavuna'
-}());
-console.log(casa);
+
+});
