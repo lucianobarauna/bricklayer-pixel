3b92569789262b6ea730903d277561b891066b57
Luciano Barauna [Fattoria] <luciano@FattoriaWebs-MacBook-Pro-2.local>
2016-12-12 14:34:50 -0200

    Atualizado com css as configs da task build code

--- tasks/taskBuildCode.js
+++ tasks/taskBuildCode.js
@@ -3,9 +3,9 @@ module.exports = function (gulp, plugins, config) {
     gulp.task("buildCode", function(){
         return gulp.src(config.htmlSrcPath)
                    .pipe(plugins.usemin({
-                        copyDist:[],
                         jsLib:[plugins.uglify],
                         jsScript:[plugins.uglify],
+                        css:[plugins.autoprefixer, plugins.cssmin]
 
                    }))
                    .pipe(gulp.dest(config.srcDist));
