94976c5f084607a9057517d272c78da5ce5d5ab0
Luciano Baraúna <baraunaluciano@gmail.com>
2017-11-28 14:13:33 -0200

    include sass but error auto reload in pug continue

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,40 @@
 const gulp = require('gulp')
 const pug = require('gulp-pug')
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
 const browserSync = require('browser-sync').create()
 
-
-// const pathSrc = {
-//     htmlTemplate: 'src/pug/*.pug'
-// }
-
-// const pathPublic = {
-//     baseDir: 'public/',
-//     html: 'public/*.html'
-
-// }
-
-
-// gulp.task('taskHtml', () => {
-//     return gulp.src('src/pug/*.pug')
-//                .pipe(pug({
-//                    pretty: '\t',
-//                    compileDebug: false
-//                }))
-//             .pipe(gulp.dest('public/'))
-// })
-
-
-// gulp.task('watch', ['taskHtml'], (done) => {
-//     browserSync.reload();
-//     done();
-// })
-
-// // Server
-// gulp.task('taskServer', ['taskHtml'], () => {
-//     browserSync.init({
-//         server: {
-//             baseDir: './public'
-//         }
-//     });
-
-//     gulp.watch('src/pug/*.pug', ['watch']);
-//     gulp.watch("public/*.html").on('change', browserSync.reload);
-
-
-// });
-
-// gulp.task('default', ['taskServer'])
-
-gulp.task('html', function taskHtml(){
-    return gulp.src('./src/pug/*.pug')
+gulp.task( 'taskPug', function taskPug(){
+    return gulp.src('src/pug/*.pug')
                .pipe(pug({
-                //    pretty: '\t'
-               }))
-               .pipe(gulp.dest('./public/'))
+                            pretty: '\t',
+                            compileDebug: false
+                    }))
+               .pipe( gulp.dest('public/') )
+               .pipe(browserSync.stream({stream:true}) )
+});
+
+gulp.task('taskSass', function taskSass(){
+    return gulp.src('src/sass/*.scss')
+               .pipe(sourcemaps.init())
+               .pipe(
+                    sass({
+                          outputStyle: 'compressed',
+                          sourceComments: 'map'
+                        }).on('error', sass.logError)
+                )
+                .pipe(sourcemaps.write('./'))
+                .pipe(gulp.dest('public/css/'))
+                .pipe(browserSync.stream({stream:true}))
 })
 
-gulp.task('html-watch', ['html'], function(done){
-    browserSync.reload()
-    console.log(done)
-    done()
-})
-
-gulp.task('default', ['html'] ,function(){
-
+gulp.task( 'default', ['taskPug', 'taskSass'], function startServe(){
     browserSync.init({
-        server: './public'
-    })
+        server: 'public'
+    });
 
-    gulp.watch('./public/*.html', ['html-watch']);
-})
+    gulp.watch('src/pug/*.pug', ['taskPug', browserSync.reload]);
+    gulp.watch('src/sass/*.scss', ['taskSass', browserSync.reload]);
+    gulp.watch('public/**/*').on( 'change', browserSync.reload )
+
+});
