e04804560d703f8244a6014c203f8e6ef90b2b41
Luciano Baraúna <baraunaluciano@gmail.com>
2017-11-28 17:44:51 -0200

    error in server task

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,36 @@ const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
 const browserSync = require('browser-sync').create()
 
-gulp.task( 'taskPug', function taskPug(){
-    return gulp.src('src/pug/*.pug')
-               .pipe(pug({
-                            pretty: '\t',
-                            compileDebug: false
-                    }))
-               .pipe( gulp.dest('public/') )
-               .pipe(browserSync.stream({stream:true}) )
-});
+// gulp.task( 'taskPug', function taskPug(){
+//     return gulp.src('src/pug/*.pug')
+//                .pipe(pug({
+//                             pretty: '\t',
+//                             compileDebug: false
+//                     }))
+//                .pipe( gulp.dest('public/') )
+//                .pipe(browserSync.stream({stream:true}) )
+// });
+
+// gulp.task( 'default', ['taskPug', 'taskSass'], function startServe(){
+//     browserSync.init({
+//         server: './public'
+//     });
+//     gulp.watch('src/sass/*.scss', ['taskPug']);
+//     gulp.watch('src/sass/*.scss', ['taskSass']);
+//     gulp.watch('public/**/*').on( 'change', browserSync.reload )
+
+// });
+
+
+gulp.task('taskPug', () => {
+  return gulp.src('src/pug/*.pug')
+  .pipe(pug({
+      pretty: '\t',
+      compileDebug: false
+  }))
+  .pipe(gulp.dest('public/'))
+  .pipe(browserSync.reload())
+})
 
 gulp.task('taskSass', function taskSass(){
     return gulp.src('src/sass/*.scss')
@@ -28,13 +49,17 @@ gulp.task('taskSass', function taskSass(){
                 .pipe(browserSync.stream({stream:true}))
 })
 
-gulp.task( 'default', ['taskPug', 'taskSass'], function startServe(){
-    browserSync.init({
-        server: 'public'
-    });
+gulp.task('create-server', () => browserSync.init({
+  server: {
+      baseDir: './public/'
+  }
+}))
 
-    gulp.watch('src/pug/*.pug', ['taskPug']);
-    gulp.watch('src/sass/*.scss', ['taskSass']);
-    gulp.watch('public/**/*').on( 'change', browserSync.reload )
-
-});
+gulp.task('default', [
+  'create-server',
+  'taskPug',
+  'taskSass'
+], () => {
+  return gulp.watch('src/pug/*.pug', ['taskPug'])
+             .watch('src/sass/*.scss', ['taskSass'])
+})
