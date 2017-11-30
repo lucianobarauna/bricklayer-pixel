dbcccc790f7c9b8c24dd46c4eadd1639edff104b
Luciano Baraúna <baraunaluciano@gmail.com>
2017-11-30 16:18:33 -0200

    auto reload funcioando

--- gulpfile.js
+++ gulpfile.js
@@ -5,76 +5,41 @@ const plumber = require('gulp-plumber')
 const pug = require('gulp-pug')
 const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
-
 const reload = browserSync.reload
 
+gulp.task('taskPug', () => {
+  gulp.src('./src/pug/*.pug')
+      .pipe(plumber())
+      .pipe(pug({
+        pretty: '\t',
+        compileDebug: true
+      }))
+      .pipe(gulp.dest('./dist/'))
+      .pipe(reload({stream: true}))
+})
 
-// Funcionando
-// gulp.task('taskHtml', () => {
-    //   gulp.src('./src/*.html')
-    //       .pipe(gulp.dest('./dist/'))
-    // });
+gulp.task('taskPug:watch', ['taskPug'], reload);
 
 gulp.task('taskSass', () => {
     gulp.src('./src/assets/sass/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(
-        sass().on('error', sass.logError)
-    )
+    .pipe(sass())
+    .on('error', sass.logError)
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist/css'))
-    //   .pipe(browserSync.stream())
     .pipe(reload({stream: true}))
-
 })
-
-gulp.task('taskHtml', () => {
-    gulp.src('./src/*.pug')
-    .pipe(plumber())
-    .pipe(pug({
-      pretty: '\t',
-      compileDebug: true
-    }))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(reload({stream: true}))
-});
-
-gulp.task('rebuild', ['taskHtml'], function () {
-  reload();
-});
 
 gulp.task('taskClearDist', () => {
   del('./dist/**/*')
 })
 
-gulp.task('watch', () => {
-  gulp.watch('src/assets/sass/**/*', ['taskSass'])
-  gulp.watch('src/**/*.pug', ['rebuild'])
-})
-
-
-// gulp.task('taskServer', ['taskSass', 'taskHtml'], () => {
-//   browserSync.init({
-//     server: {
-//       baseDir: "./dist"
-//     }
-//   })
-//   gulp.watch("src/assets/sass/**/*.scss", ['taskSass'])
-//   gulp.watch("src/**/*.pug", ['taskHtml'])
-//   gulp.watch("./src/**/*").on('change', reload);
-// })
-
 gulp.task('taskServer', () => {
-  browserSync.init({
-    server: {
-      baseDir: "./dist"
-    }
-  })
+  browserSync.init({ server: './dist' });
+  gulp.watch('./src/assets/sass/**/*.scss', ['taskSass']);
+  gulp.watch('./src/pug/**/*.pug', ['taskPug:watch']);
 })
 
-gulp.task('taskDist', ['taskClearDist'], () => {
-  gulp.start('taskSass', 'taskHtml')
-})
+gulp.task('default', ['taskServer', 'taskSass', 'taskPug', 'taskClearDist']);
 
-gulp.task('default', ['taskDist', 'taskServer', 'watch'])
-// gulp.task('default', ['taskBuild', 'taskWatch', 'taskBrowserSync'])
+
