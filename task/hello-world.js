7a8ea24a18a081a23ff428d7d04e7db89bb048c6
Luciano Baraúna <baraunaluciano@gmail.com>
2021-03-03 19:13:03 -0300

    update text

--- task/hello-world.js
+++ task/hello-world.js
@@ -8,4 +8,4 @@ log(`${emoji.get('rocket')} ${chalk.blue('Olá mundo o foguete subiu')}`);
 
 log(`${emoji.get('boom')} ${chalk.red('Mais ele explodiu!! ')}`);
 
-log(`${emoji.get('muscle')} ${chalk.green('Mas temos força...Isso foi um exemplo de script postinstall')}`);
+log(`${emoji.get('muscle')} ${chalk.green('Mas temos força...Isso foi um exemplo de script hook')}`);
